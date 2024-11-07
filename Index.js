ce8f1201b389f0605566d5f336940f13bbc2e800
David Chaux <dchaux980@gmail.com>
2024-11-07 15:19:41 -0500

    commit2

--- Index.js
+++ Index.js
@@ -2,19 +2,29 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const port = 3000;
+const colors = require("colors");
 
-app.get('/Login', (require, res) => {
-    res.sendFile(path.join(__dirname,"Login.html"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/Login', (req, res) => {
+    res.render("Login");     //Esta es la ruta que lleva al login//
+    console.log("Accediendo a Login".bgBlue.black);
 });
 
-app.get('/Bienvenido', (require, res) => {
-    res.sendFile(path.join(__dirname,"Bienvenido.html"));
+app.get('/Bienvenido', (req, res) => {
+    res.render("Bienvenido");  //Esta es la ruta que va al Bienvenido//
+    console.log("Accediendo a Bienvenido".bgRed.black);
 });
 
-app.get('/Datos', (require, res) => {
-    res.sendFile(path.join(__dirname,"Datos.html"));
+app.get('/Datos', (req, res) => {
+    res.render("Datos");   //Esta es la ruta que val a Datos//
+    console.log("Accediendo a Datos".bgGreen.black);
 });
 
 app.listen(port, () => {
-    console.log("abriendo servidor en puerto 3000");
+    console.log("abriendo servidor en puerto 3000");   //Esta es la ruta para abrir el servidor en el puerto 3000//
 });
